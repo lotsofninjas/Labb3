f9e2f1f8fd2d677e4766b85d1adaa693f6071d85
lotsofninjas <PelleB.86@gmail.com>
2023-11-29 10:59:41 +0100

    första

---
+++ Labb 3/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_3
+{
+    public class Person
+    {
+        public DateTime DateOfBirth { get; set; }
+        public string EyeColor { get; set; }
+        public GenderInfo Gender { get; set; }
+        public HairInfo Hair { get; set; }
+        
+    }
+}
---
+++ Labb 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_3
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}
